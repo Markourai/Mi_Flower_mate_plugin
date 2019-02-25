64190dae172983759955e65519dafaa0413841f5
Markourai <13946579+Markourai@users.noreply.github.com>
2019-02-25 16:39:16 +0100

    update script with good mac address

--- update_MiFlora.sh
+++ update_MiFlora.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Miflora Pointsettia
 python3 update_MiFlora.py -a "C4:7C:8D:67:4B:28" -m "117" -t "118" -l "119" -f "120" -c "Pointsettia" &
-# Miflora Bonsai
-python3 update_MiFlora.py -a "C4:7C:8D:62:47:D0" -m "107" -t "108" -l "109" -f "110" -c "Bonsai" &
+# Miflora Palmier
+python3 update_MiFlora.py -a "C4:7C:8D:67:4B:0E" -m "107" -t "108" -l "109" -f "110" -c "Palmier" &
 # Miflora Olivier
-python3 update_MiFlora.py -a "C4:7C:8D:62:48:40" -m "121" -t "122" -l "123" -f "124" -c "Olivier" & 
+python3 update_MiFlora.py -a "C4:7C:8D:67:4C:40" -m "121" -t "122" -l "123" -f "124" -c "Olivier" &
